ae6e6ed8144b04db69f4ef0263fa398da6e795f6
sriharikunchala18 <sriharikunchalla@gmail.com>
2025-11-16 16:35:57 +0530

    Fix import for RecursiveCharacterTextSplitter

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import PyPDF2
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def extract_text_from_pdf(pdf_file):
     """
