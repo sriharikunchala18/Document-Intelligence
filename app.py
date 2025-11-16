10ae40a8ffe2536e552097944f98f687dfe85017
sriharikunchala18 <sriharikunchalla@gmail.com>
2025-11-16 20:30:38 +0530

    Fix import for RetrievalQA

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFacePipeline
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa import RetrievalQA
 
 from langchain_core.documents import Document
 from transformers import pipeline
